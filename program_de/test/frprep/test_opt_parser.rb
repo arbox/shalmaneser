445c55a7d52c81d9976f0f27e3e5605598f2675f
Andrei Beliankou <arbox@yandex.ru>
2011-11-13 12:37:06 +0000

    Added helper methods.

--- program_de/test/frprep/test_opt_parser.rb
+++ program_de/test/frprep/test_opt_parser.rb
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 require 'test/unit'
+require 'stringio' # for helper methods
 require 'frprep/opt_parser'
 
 include FrPrep
@@ -25,4 +26,35 @@ class TestOptParser < Test::Unit::TestCase
     assert(return_value.instance_of?(FrPrepConfigData))
   end
 
+end
+################################################################################
+  # It is a helper method, many testable units provide some verbose output
+  # to stderr and/or stdout. It is usefull to suppress any kind of verbosity.
+  def quietly(&b)
+    begin
+      orig_stderr = $stderr.clone
+      orig_stdout = $stdout.clone
+      $stderr.reopen(File.new('/dev/null', 'w'))
+      $stdout.reopen(File.new('/dev/null', 'w'))
+      b.call
+    ensure
+      $stderr.reopen(orig_stderr)
+      $stdout.reopen(orig_stdout)
+    end
+  end
+
+  # It is a helper method for handling stdout and stderr as strings.
+  def intercept_output
+    orig_stdout = $stdout
+    orig_stderr = $stderr
+    $stdout = StringIO.new
+    $stderr = StringIO.new
+
+    yield
+
+    return $stdout.string, $stderr.string
+  ensure
+    $stdout = orig_stdout
+    $stderr = orig_stderr
+  end
 end
