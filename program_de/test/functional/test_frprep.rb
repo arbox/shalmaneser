67f7007b02e4860e03043c39b030c95a39c5e204
Andrei Beliankou <arbox@yandex.ru>
2011-11-13 16:03:34 +0000

    Corrected the invokations options, added <lib> into the load path.

--- program_de/test/functional/test_frprep.rb
+++ program_de/test/functional/test_frprep.rb
@@ -5,9 +5,7 @@ require 'test/unit'
 class TestFrprep < Test::Unit::TestCase
 
   def test_for_errors
-    assert(
-           system('ruby bin/frprep -e SampleExperimentFiles.salsa/prp_test.salsa'),
-           "FrPrep is doing bad, you've just broken something!"
-           )
+    val = system('ruby -I lib bin/frprep -e SampleExperimentFiles.salsa/prp_test.salsa')
+    assert(val, "FrPrep is doing bad, you've just broken something!")
   end
 end
