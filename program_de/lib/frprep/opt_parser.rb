e555de1a379b631af6c8f06d7f73cf99b3ddf2f0
Andrei Beliankou <arbox@yandex.ru>
2011-11-13 16:05:17 +0000

    Implemented a simple option parser for frprep.

--- program_de/lib/frprep/opt_parser.rb
+++ program_de/lib/frprep/opt_parser.rb
@@ -3,14 +3,88 @@
 
 # AB, 2010-11-25
 
-require 'getoptlong'
 require 'optparse'
+require 'common/FrPrepConfigData'
+require 'common/SynInterfaces'
 module FrPrep
 
   # This class parses the option for FRPrep.
   class OptParser
 
-    
+    # Main class method.
+    # OP expects cmd_args to be an array like ARGV.
+    def self.parse(cmd_args)
+      @prg_name = 'frprep'
+      @@options = {}
+      
+      parser = create_parser
+      
+      # If no options provided print the help.
+      if cmd_args.empty?
+        $stderr.puts('You have to provide some options.',
+                    "Please start with <#{@prg_name} --help>.")
+        exit(1)
+      end
+      
+      # Parse ARGV and provide the options hash.
+      # Check if everything is correct and handle exceptions
+      begin
+        parser.parse(cmd_args)
+      rescue OptionParser::InvalidArgument => e
+        arg = e.message.split.last
+        $stderr.puts "The provided argument #{arg} is currently not supported!"
+        $stderr.puts "Please colsult <#{@prg_name} --help>."
+        exit(1)
+      rescue OptionParser::InvalidOption => e
+        $stderr.puts "You have provided an #{e.message}."
+        $stderr.puts "Please colsult <#{@prg_name} --help>."
+        exit(1)
+      rescue
+        raise
+      end
+
+      
+      exp = FrPrepConfigData.new(@@options[:exp_file])
+
+      # AB: this stuff should be move into FrPrepConfigData.
+      # sanity checks
+      unless exp.get("prep_experiment_ID") =~ /^[A-Za-z0-9_]+$/
+        raise "Please choose an experiment ID consisting only of the letters A-Za-z0-9_."
+      end
+
+      SynInterfaces.check_interfaces_abort_if_missing(exp)
+
+      exp
+    end
+
+    private
+    def self.create_parser
+      OptionParser.new do |opts|
+        opts.banner = 'Usage: frprep OPTIONS'
+        
+        opts.separator ''
+        opts.separator 'Program specific options:'
+        
+        opts.on('-e', '--expfile FILENAME',
+                'Provide the path to an experiment file,',
+                'to test Yanser you can use <YahooDemo> as the APPID,',
+                'think in this case on limitations placed by Yahoo.',
+                'This option is required!'
+                ) do |exp_file|
+          @@options[:exp_file] = File.expand_path(exp_file)
+        end
+
+        opts.separator ''
+        opts.separator 'Common options:'
+        
+        opts.on_tail('-h', '--help', 'Show the help message.') do
+          puts opts
+          exit
+        end
+        
+      end
+    end
+
     def usage
       $stderr.puts "
 FrPrep: Preprocessing for Fred and Rosy
@@ -19,8 +93,7 @@ FrPrep: Preprocessing for Fred and Rosy
 Usage:
 ----------------
 
-ruby frprep.rb --help|-h
-  Gets you this text.
+
 
 
 ruby frprep.rb --expfile|-e <e>
