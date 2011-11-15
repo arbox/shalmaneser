4b646659d2744d9e7936536ffc377adb428f2a4c
Andrei Beliankou <arbox@yandex.ru>
2011-11-15 17:15:23 +0000

    Added tests for fred training.

--- program_de/test/functional/test_fred.rb
+++ program_de/test/functional/test_fred.rb
@@ -4,11 +4,18 @@ require 'test/unit'
 
 class TestFred < Test::Unit::TestCase
 
-  def test_for_errors
+  def test_fred_testing
     val = system('ruby -I lib bin/fred -t featurize -e SampleExperimentFiles.salsa/fred_test.salsa -d test')
     assert(val, "Fred is doing bad, you've just broken something!")
     val = system('ruby -I lib bin/fred -t test -e SampleExperimentFiles.salsa/fred_test.salsa')
     assert(val, "Fred is doing bad, you've just broken something!")
     
+  end
+
+  def test_fred_training
+    val = system('ruby -I lib bin/fred -t featurize -e SampleExperimentFiles.salsa/fred_train.salsa -d train')
+    assert(val, "Fred is doing bad, you've just broken something!")
+    val = system('ruby -I lib bin/fred -t train -e SampleExperimentFiles.salsa/fred_train.salsa')
+    assert(val, "Fred is doing bad, you've just broken something!")
   end
 end
