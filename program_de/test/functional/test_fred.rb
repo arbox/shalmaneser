3c144735a8342eaaf21afae7a48f67fe25cfbeb6
Andrei Beliankou <arbox@yandex.ru>
2011-11-15 20:55:07 +0000

    Refactored <fred_test> to use the helper.

--- program_de/test/functional/test_fred.rb
+++ program_de/test/functional/test_fred.rb
@@ -1,21 +1,25 @@
 # -*- encoding: utf-8 -*-
 
 require 'test/unit'
+require 'functional/functional_test_helper'
 
 class TestFred < Test::Unit::TestCase
 
+  include FunctionalTestHelper
+
+  def setup
+    @msg = "Fred is doing bad, you've just broken something!"
+    @test_file = 'test/functional/sample_experiment_files/fred_test.salsa'
+    @train_file = 'test/functional/sample_experiment_files/fred_train.salsa'
+  end
+
   def test_fred_testing
-    val = system('ruby -I lib bin/fred -t featurize -e SampleExperimentFiles.salsa/fred_test.salsa -d test')
-    assert(val, "Fred is doing bad, you've just broken something!")
-    val = system('ruby -I lib bin/fred -t test -e SampleExperimentFiles.salsa/fred_test.salsa')
-    assert(val, "Fred is doing bad, you've just broken something!")
-    
+    execute("ruby -I lib bin/fred -t featurize -e #{@test_file} -d test")
+    execute("ruby -I lib bin/fred -t test -e #{@test_file}")
   end
 
   def test_fred_training
-    val = system('ruby -I lib bin/fred -t featurize -e SampleExperimentFiles.salsa/fred_train.salsa -d train')
-    assert(val, "Fred is doing bad, you've just broken something!")
-    val = system('ruby -I lib bin/fred -t train -e SampleExperimentFiles.salsa/fred_train.salsa')
-    assert(val, "Fred is doing bad, you've just broken something!")
+    execute("ruby -I lib bin/fred -t featurize -e #{@train_file} -d train")
+    execute("ruby -I lib bin/fred -t train -e #{@train_file}")
   end
 end
