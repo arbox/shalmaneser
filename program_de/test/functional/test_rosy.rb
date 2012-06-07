781284afbe385ac9c84d7b4ff84453dad91354c5
Andrei Beliankou <arbox@yandex.ru>
2012-06-07 18:36:58 +0000

    Standalone tests for Rosy.

--- program_de/test/functional/test_rosy.rb
+++ program_de/test/functional/test_rosy.rb
@@ -11,10 +11,10 @@ class TestRosy < Test::Unit::TestCase
   end
   def test_rosy_testing
     create_exp_file(ROSY_TEST_FILE)
-    create_exp_file(PRP_TEST_FILE)
+    create_exp_file(PRP_TEST_FILE_ROSY_STD)
     execute("ruby -rubygems -I lib bin/rosy -t featurize -e #{ROSY_TEST_FILE} -d test")
     execute("ruby -rubygems -I lib bin/rosy -t test -e #{ROSY_TEST_FILE}")
     remove_exp_file(ROSY_TEST_FILE)
-    remove_exp_file(PRP_TEST_FILE)
+    remove_exp_file(PRP_TEST_FILE_ROSY_STD)
   end
 end
