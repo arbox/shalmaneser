2936f5469784a7b752e7f054eac7347e02d4ae37
Andrei Beliankou <arbox@yandex.ru>
2012-06-08 18:29:42 +0200

    Split the tests for Fred, they have only one assertion now.

--- program_de/test/functional/test_fred.rb
+++ program_de/test/functional/test_fred.rb
@@ -13,19 +13,33 @@ class TestFred < Test::Unit::TestCase
     @train_file = FRED_TRAIN_FILE
   end
 
-  def test_fred_testing
+  def test_fred_testing_featurization
     create_exp_file(@test_file)
     create_exp_file(PRP_TEST_FILE_STD)
     execute("ruby -I lib bin/fred -t featurize -e #{@test_file} -d test")
+    remove_exp_file(@test_file)
+    remove_exp_file(PRP_TEST_FILE_STD)
+  end
+
+  def test_fred_testing_tests
+    create_exp_file(@test_file)
+    create_exp_file(PRP_TEST_FILE_STD)
     execute("ruby -I lib bin/fred -t test -e #{@test_file}")
     remove_exp_file(@test_file)
     remove_exp_file(PRP_TEST_FILE_STD)
   end
 
-  def test_fred_training
+  def test_fred_training_featurization
     create_exp_file(@train_file)
     create_exp_file(PRP_TRAIN_FILE_STD)
     execute("ruby -I lib bin/fred -t featurize -e #{@train_file} -d train")
+    remove_exp_file(@train_file)
+    remove_exp_file(PRP_TRAIN_FILE_STD)
+  end
+
+  def test_fred_training_train
+    create_exp_file(@train_file)
+    create_exp_file(PRP_TRAIN_FILE_STD)
     execute("ruby -I lib bin/fred -t train -e #{@train_file}")
     remove_exp_file(@train_file)
     remove_exp_file(PRP_TRAIN_FILE_STD)
