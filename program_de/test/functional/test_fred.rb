cbb63292ede363a09771e74807c012623bfda39b
Andrei Beliankou <arbox@yandex.ru>
2012-06-09 17:39:49 +0200

    Applied naming conventions to sample experiment files.

--- program_de/test/functional/test_fred.rb
+++ program_de/test/functional/test_fred.rb
@@ -15,33 +15,33 @@ class TestFred < Test::Unit::TestCase
 
   def test_fred_testing_featurization
     create_exp_file(@test_file)
-    create_exp_file(PRP_TEST_FILE_STD)
+    create_exp_file(PRP_TEST_FILE_FRED_STD)
     execute("ruby -I lib bin/fred -t featurize -e #{@test_file} -d test")
     remove_exp_file(@test_file)
-    remove_exp_file(PRP_TEST_FILE_STD)
+    remove_exp_file(PRP_TEST_FILE_FRED_STD)
   end
 
   def test_fred_testing_tests
     create_exp_file(@test_file)
-    create_exp_file(PRP_TEST_FILE_STD)
+    create_exp_file(PRP_TEST_FILE_FRED_STD)
     execute("ruby -I lib bin/fred -t test -e #{@test_file}")
     remove_exp_file(@test_file)
-    remove_exp_file(PRP_TEST_FILE_STD)
+    remove_exp_file(PRP_TEST_FILE_FRED_STD)
   end
 
   def test_fred_training_featurization
     create_exp_file(@train_file)
-    create_exp_file(PRP_TRAIN_FILE_STD)
+    create_exp_file(PRP_TRAIN_FILE_FRED_STD)
     execute("ruby -I lib bin/fred -t featurize -e #{@train_file} -d train")
     remove_exp_file(@train_file)
-    remove_exp_file(PRP_TRAIN_FILE_STD)
+    remove_exp_file(PRP_TRAIN_FILE_FRED_STD)
   end
 
   def test_fred_training_train
     create_exp_file(@train_file)
-    create_exp_file(PRP_TRAIN_FILE_STD)
+    create_exp_file(PRP_TRAIN_FILE_FRED_STD)
     execute("ruby -I lib bin/fred -t train -e #{@train_file}")
     remove_exp_file(@train_file)
-    remove_exp_file(PRP_TRAIN_FILE_STD)
+    remove_exp_file(PRP_TRAIN_FILE_FRED_STD)
   end
 end
