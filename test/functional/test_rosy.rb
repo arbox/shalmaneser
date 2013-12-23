bc5dd63b59b6ac20db826f9b55a75f2d338961d2
Andrei Beliankou <arbox@yandex.ru>
2013-12-23 23:17:47 +0100

    Added tests for Rosy: split and onestap training.

--- test/functional/test_rosy.rb
+++ test/functional/test_rosy.rb
@@ -9,6 +9,7 @@ class TestRosy < Test::Unit::TestCase
   def setup
     @msg = "Rosy is doing bad, you've just broken something!"
   end
+
   def test_rosy_testing
     create_exp_file(ROSY_TEST_FILE)
     create_exp_file(PRP_TEST_FILE_ROSY_STD)
@@ -16,5 +17,24 @@ class TestRosy < Test::Unit::TestCase
     execute("ruby -rubygems -I lib bin/rosy -t test -e #{ROSY_TEST_FILE}")
     remove_exp_file(ROSY_TEST_FILE)
     remove_exp_file(PRP_TEST_FILE_ROSY_STD)
+  end
+
+  def test_rosy_training
+    create_exp_file(ROSY_TRAIN_FILE)
+    create_exp_file(PRP_TRAIN_FILE_ROSY_STD)
+    execute("ruby -rubygems -I lib bin/rosy -t featurize -e #{ROSY_TRAIN_FILE} -d train")
+    execute("ruby -rubygems -I lib bin/rosy -t train -e #{ROSY_TRAIN_FILE} -s argrec")
+    execute("ruby -rubygems -I lib bin/rosy -t train -e #{ROSY_TRAIN_FILE} -s arglab")
+    remove_exp_file(ROSY_TRAIN_FILE)
+    remove_exp_file(PRP_TRAIN_FILE_ROSY_STD)
+  end
+
+  def test_rosy_training_onestep
+    create_exp_file(ROSY_TRAIN_FILE)
+    create_exp_file(PRP_TRAIN_FILE_ROSY_STD)
+    execute("ruby -rubygems -I lib bin/rosy -t featurize -e #{ROSY_TRAIN_FILE} -d train")
+    execute("ruby -rubygems -I lib bin/rosy -t train -e #{ROSY_TRAIN_FILE} -s onestep")
+    remove_exp_file(ROSY_TRAIN_FILE)
+    remove_exp_file(PRP_TRAIN_FILE_ROSY_STD)
   end
 end
