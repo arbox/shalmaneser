5333de845e511108483c8f72bbe8b87033f08662
Andrei Beliankou <arbox@yandex.ru>
2015-09-08 22:43:27 +0200

    Code style improvements.

--- lib/common/SalsaTigerXMLHelper.rb
+++ lib/common/SalsaTigerXMLHelper.rb
@@ -3,7 +3,7 @@
 # Static helper methods for SalsaTigerRegXML:
 
 # - provide header and footer for Salsa/Tiger XML files
-# - escape and unescape HTML entities 
+# - escape and unescape HTML entities
 #
 # changed KE nov 05:
 # many methods moved to FrprepHelper
@@ -14,86 +14,79 @@ require "common/Parser"
 require "tempfile"
 
 class SalsaTigerXMLHelper
-
-
   ###
   # get header of SalsaTigerXML files (as string)
-  def SalsaTigerXMLHelper.get_header 
-    
+  def self.get_header
+
     header = <<ENDOFHEADER
 <?xml version="1.0" encoding="UTF-8"?>
   <corpus corpusname="corpus" target="">
-	<head>
-		<meta>
-			<format>
-			NeGra format, version 3</format>
-		</meta>
-		<frames xmlns="http://www.clt-st.de/framenet/frame-database">
-		</frames>
-		<wordtags xmlns="http://www.clt-st.de/salsa/wordtags">
-		</wordtags>
-		<flags>
-		</flags>
-		<annotation>
-			<edgelabel>
-			</edgelabel>
-			<secedgelabel>
-			</secedgelabel>
-		</annotation>
-	</head>
-	<body>
+        <head>
+                <meta>
+                        <format>
+                        NeGra format, version 3</format>
+                </meta>
+                <frames xmlns="http://www.clt-st.de/framenet/frame-database">
+                </frames>
+                <wordtags xmlns="http://www.clt-st.de/salsa/wordtags">
+                </wordtags>
+                <flags>
+                </flags>
+                <annotation>
+                        <edgelabel>
+                        </edgelabel>
+                        <secedgelabel>
+                        </secedgelabel>
+                </annotation>
+        </head>
+        <body>
 ENDOFHEADER
-    
-    return header
-    
+
+    header
   end
-  
+
   ###
   # get footer of SALSATigerXML files (as string)
-  def SalsaTigerXMLHelper.get_footer
-    
+  def self.get_footer
+
     footer = <<ENDOFFOOTER
-	</body>
+        </body>
 </corpus>
 ENDOFFOOTER
-    
+
     return footer
   end
-  
 
-  
-# escape and unescape strings for representation in XML
-  
+  # escape and unescape strings for representation in XML
+
   @@replacements = [
-#  ["&apos;&apos;","&quot;"], # added by ines (09/03/09), might cause problems for unescape???
-  ["&","&amp;"], # must be first for escaping, last for unescaping
-  ["<","&lt;"],
-  [">", "&gt;"],
-  ["\"","&apos;&apos;"],
-#  ["\"","&quot;"],
-#  ["\'\'","&quot;"],
-#  ["\`\`","&quot;"],
-  ["\'","&apos;"],
-  ["\`\`","&apos;&apos;"],
-#  ["''","&apos;&apos;"]
+    #  ["&apos;&apos;","&quot;"], # added by ines (09/03/09), might cause problems for unescape???
+    ["&","&amp;"], # must be first for escaping, last for unescaping
+    ["<","&lt;"],
+    [">", "&gt;"],
+    ["\"","&apos;&apos;"],
+    #  ["\"","&quot;"],
+    #  ["\'\'","&quot;"],
+    #  ["\`\`","&quot;"],
+    ["\'","&apos;"],
+    ["\`\`","&apos;&apos;"],
+    #  ["''","&apos;&apos;"]
   ]
 
+  def self.escape(string)
+    @@replacements.each do |unescaped, escaped|
+      string.gsub!(unescaped, escaped)
+    end
 
+    string
+  end
 
-def SalsaTigerXMLHelper.escape(string)
-  @@replacements.each {|unescaped,escaped|
-    string.gsub!(unescaped,escaped)
-  }
-  return string
-end
+  def self.unescape(string)
+    # reverse replacements to replace &amp last
+    @@replacements.reverse_each do |unescaped, escaped|
+      string.gsub!(escaped, unescaped)
+    end
 
-def SalsaTigerXMLHelper.unescape(string)
-  # reverse replacements to replace &amp last
-  @@replacements.reverse.each {|unescaped,escaped| 
-    string.gsub!(escaped,unescaped)
-  }
-  return string
-end
-
-
+    string
+  end
 end
