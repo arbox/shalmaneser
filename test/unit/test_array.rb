1bf1ec6254da6162e2089e1ee5e36c7bfc1a657e
Andrei Beliankou <arbox@yandex.ru>
2015-10-06 19:13:38 +0200

    Added dummy specs for monkey methods in Array.

---
+++ test/unit/test_array.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require 'minitest/spec'
+
+require 'common/monkey_patching/array'
+
+describe Array do
+  it 'can be created with no arguments' do
+    [].must_be_instance_of Array
+  end
+
+  it 'should count special occurrences'
+
+  it 'should count special occurrences'
+
+  describe '#sample' do
+    it 'should return a sample'
+  end
+
+  it 'should provide indices for blocks'
+end
